aa558507dc56e421873fcdc7502bb3f627089578
(no author) <(no author)@c941bd0c-2326-0410-aed5-8f551ad5c796>
2007-07-11 19:44:40 +0000

    Goodbye - what SCM is for, right?
    git-svn-id: svn+ssh://cheer/svn/experimental@24 c941bd0c-2326-0410-aed5-8f551ad5c796

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-glib-gettextize --copy --force
-libtoolize --automake --copy --force
-intltoolize --automake --copy --force
-aclocal
-autoconf --force
-autoheader --force
-automake --add-missing --copy --force-missing --foreign
