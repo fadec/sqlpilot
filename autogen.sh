35c4dacb991a74776bb72bda242b7b5405411670
(no author) <(no author)@c941bd0c-2326-0410-aed5-8f551ad5c796>
2007-07-12 13:32:04 +0000

    git-svn-id: svn+ssh://cheer/svn/experimental@26 c941bd0c-2326-0410-aed5-8f551ad5c796

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-glib-gettextize --copy --force
-libtoolize --automake --copy --force
-intltoolize --automake --copy --force
-aclocal-1.9
-autoconf --force
-autoheader --force
-automake-1.9 --add-missing --copy --force-missing --foreign
