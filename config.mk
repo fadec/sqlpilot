eabab03e4fa8a8664f2d63a083b749d051479698
Sam Danielson <samdanielson@gmail.com>
2008-06-06 14:11:17 -0400

    fixed tabs->spaces in GPL notices, added README

--- config.mk
+++ config.mk
@@ -1,18 +1,18 @@
 ########################################################################
-# Copyright (C) 2008  Sam Danielson				       #
+# Copyright (C) 2008  Sam Danielson                                    #
 #                                                                      #
-# This file is part of Sqlpilot.				       #
-# 								       #
+# This file is part of Sqlpilot.                                       #
+#                                                                      #
 # Sqlpilot is free software: you can redistribute it and/or modify     #
 # it under the terms of the GNU General Public License as published by #
 # the Free Software Foundation, either version 3 of the License, or    #
-# (at your option) any later version.				       #
-# 								       #
-# Sqlpilot is distributed in the hope that it will be useful,	       #
+# (at your option) any later version.                                  #
+#                                                                      #
+# Sqlpilot is distributed in the hope that it will be useful,          #
 # but WITHOUT ANY WARRANTY; without even the implied warranty of       #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	       #
-# GNU General Public License for more details.			       #
-# 								       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
 # You should have received a copy of the GNU General Public License    #
 # along with Sqlpilot.  If not, see <http://www.gnu.org/licenses/>.    #
 ########################################################################
@@ -23,12 +23,11 @@ VERSION_MINOR = 0
 VERSION_TINY  = 0
 
 USING_HILDON=false
-USING_GTK_BUILDER=false
+USING_GTK_BUILDER=true
 USING_READLINE=false
 
 # for gtk-builder-convert
 PYTHON=python2.5
-
 
 PACKAGES = gtk+-2.0 libglade-2.0 #hildon-libs
 
